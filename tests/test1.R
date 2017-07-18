3e9d3fb20f113f5abd3e93b5311d238f22da2af3
thakurdev010 <thakurdev009@gmail.com>
2017-07-18 19:03:40 +0530

    foxtrode delete

--- tests/test1.R
+++
@@ -1,9 +0,0 @@
-library(testthat)
-library(TestPackage)
-
-
-testthat::expect_that(make_filename(2012),
-                      is_identical_to("accident_2012.csv.bz2"))
-
-testthat::expect_that(make_filename("2017"),
-                      is_identical_to("accident_2017.csv.bz2"))
